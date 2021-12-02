e8479bbd66abb4ac9da5fc2a92112ea1bfa76117
ALUMNADO\vicadi <vicadi@floridauniversitaria.es>
2021-12-02 20:24:41 +0100

    Clase Programacion 02-12-2021

---
+++ PROGRAMACION/BLOQUE 2/EjemplosC/Holamundo/Holamundo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1Hola Mundo");
+            Console.WriteLine("Fecha y hora de hoy: " + DateTime.Now);
+            Console.ReadKey();
+        }
+    }
+}

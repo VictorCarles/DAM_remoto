7a6fe10227dcaad2bea5247fb30b384208d1ff07
ALUMNADO\vicadi <vicadi@floridauniversitaria.es>
2022-01-20 20:24:21 +0100

    Clase BBDD, y Prog del 20/01/2022

---
+++ PROGRAMACION/BLOQUE 3/Ejercicio Practico 2/EJ02Inicial/EJ02/Persona.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace EJ01
+{
+    class Persona
+    {
+        // Atributos (datos e informacion de la clase)
+        // Declaración: <acceso> + <modificador> + <tipo> + nombre_del_atributo.
+
+        private string nombre;
+        private int edad;
+        private bool jubilada;
+
+
+        // Constructores de la Clase
+
+        /* Primer constructor creado que recibe el nombre y la edad de la persona.
+         * La información de jubilido/a la calcula en el constructor atendiendo a si 
+         * tiene menos o más de 67 años */
+
+        public Persona(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            if (edad < 0)
+                this.edad = 0;
+            else
+                this.edad = edad;
+
+            if (edad < 67)
+                jubilada = false;
+            else
+                jubilada = true;
+
+        }
+
+
+
+        /* Segundo constructor creado que recibe el nombre y la edad de la persona.
+         * La información de jubilado/a se le pasa como parámetro al igual que nombre y edad.
+         * Este constructor sería adecuado para cuando una persona tiene más de 67 años y no
+         * se ha jubilado, o por lo contrario, se ha jubilado de forma anticipada, es decir,
+         * antes de los 67 años */
+
+        public Persona(string nombre, int edad, bool jub)
+        {
+            this.nombre = nombre.ToUpper();
+            this.edad = edad;
+            this.jubilada = jub;
+        }
+
+
+
+
+        /* Tercer constructor creado que pide los datos de la persona por teclado
+        * En este caso no recibe parámetros y sustituiría al constructor heredado
+        * de la clase Object. 
+        * El dato jubilada se pide por teclado y no se cálcula automáticamente a partir
+        * de la edad suministrada */
+
+        public Persona()
+        {
+            Console.Write("Petición de datos.");
+            Console.WriteLine("==================");
+            Console.Write("Nombre: ");
+            this.nombre = Console.ReadLine().ToUpper();
+            Console.Write("Edad: ");
+            this.edad = int.Parse(Console.ReadLine());
+            Console.Write("Jubilada (S/N): ");
+            char respuesta = Console.ReadKey().KeyChar;
+            if (respuesta == 's' || respuesta == 'S')
+                this.jubilada = true;
+            else
+                this.jubilada = false;
+
+        }
+
+
+
+
+
+        // Comportamientos (métodos y acciones de la clase).
+        // Declaración: <acceso> + <modificador> + <tipo_de_retorno> + 
+        //      nombre_del_método(lista_de_parámetros_que_recibe -si recibe-).
+
+        
+        public void setNombre(string nom)
+        {
+            this.nombre = nom;
+        }
+
+        public int getEdad()
+        {
+            return edad;
+        }
+
+        public void VerInfo()
+        {
+            Console.WriteLine("\n{0} tiene {1} años. Jubilada: {2}", nombre, edad, jubilada);
+        }
+
+
+        // Método público que incrementa la edad
+        public int Cumpleaños()
+        {
+            edad++;
+            if (edad >= 67)
+            {
+                jubilada = true;
+            } 
+            return edad;
+        }
+
+    }
+}
---
+++ PROGRAMACION/BLOQUE 3/Ejercicio Practico 2/EJ02Inicial/EJ02/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EJ01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nom;
+            int edad;
+
+            List<Persona> plantilla = new List<Persona>();
+
+
+            // Uso de los constructores
+            Console.Write("Dime un nombre: ");
+            nom = Console.ReadLine();
+            Console.Write("Dime la edad: ");
+            edad = int.Parse(Console.ReadLine());
+
+            Persona p1 = new Persona(nom, edad);
+            plantilla.Add(p1);
+            p1.VerInfo();
+            // p1.setNombre("Andres");
+            // p1.VerInfo();
+
+            Console.ReadKey();
+
+            // 2º Constructor
+
+            Persona p2 = new Persona("Ana", 56, true);
+            plantilla.Add(p2);
+            p2.VerInfo();
+
+            Console.ReadKey();
+
+            // 3º Constructor
+
+            Persona p3 = new Persona();
+            plantilla.Add(p3);
+            p3.VerInfo();
+
+            Console.ReadKey();
+
+            double promedio = 0;
+            foreach (Persona p in plantilla)
+            {
+                promedio = promedio + p.getEdad();
+            }
+            Console.WriteLine("Promedio de edades: {0:N}", promedio / plantilla.Count);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}

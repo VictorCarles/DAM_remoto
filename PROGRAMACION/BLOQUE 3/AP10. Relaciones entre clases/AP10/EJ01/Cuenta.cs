bbaaa56b67d1aa289e3b520daf80b0bac999bd2c
ALUMNADO\vicadi <vicadi@floridauniversitaria.es>
2022-01-28 17:18:13 +0100

    Clase Programación 28-01-2022

---
+++ PROGRAMACION/BLOQUE 3/AP10. Relaciones entre clases/AP10/EJ01/Cuenta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// EJERCICIO 1. Un cliente se caracteriza por su nombre, apellido y NIF.
+// El cliente puede consultar el saldo, así como ingresar y retirar dinero de sus cuentas.
+
+
+namespace EJ01
+{
+    class Cuenta
+    {
+        public int numero;
+        public double saldo;
+
+
+        
+
+
+    }
+}
